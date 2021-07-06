bf22cf8c726bf64985573fc1ff162df2acf186ac
Keiner Peñaranda <keinerjpp@gmail.com>
2021-07-05 21:06:17 -0500

    Agregando el Search

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
 import '../assets/style/App.scss';
+import Search from '../components/Search';
 
 const App = () =>(
     <div className="App">
           <Header />
+          <Search />
     </div>
     
 );
